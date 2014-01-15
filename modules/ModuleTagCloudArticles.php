15df3a077d1a0eba0e4fe8069dc3c7a388874f34
Helmut Schottmüller <helmut.schottmueller@me.com>
2014-01-15 11:11:09 +0100

    Added support for revised top tags category

--- modules/ModuleTagCloudArticles.php
+++ modules/ModuleTagCloudArticles.php
@@ -41,6 +41,7 @@ class ModuleTagCloudArticles extends \ModuleTagCloud
 		if ($this->tag_on_page_class) $this->checkForArticleOnPage = true;
 		$taglist->addNamedClass = $this->tag_named_class;
 		if ($this->restrict_to_column) $taglist->inColumn = $this->inColumn;
+		if (strlen($this->tag_topten_number) && $this->tag_topten_number > 0) $taglist->topnumber = $this->tag_topten_number;
 		if (strlen($this->tag_maxtags)) $taglist->maxtags = $this->tag_maxtags;
 		if (strlen($this->tag_buckets) && $this->tag_buckets > 0) $taglist->buckets = $this->tag_buckets;
 		if (strlen($this->tag_articles)) $taglist->articles = deserialize($this->tag_articles, TRUE);
